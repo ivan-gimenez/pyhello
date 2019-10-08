cdb1e4370692f8e3dba8169463f0a10036083d49
Ivan Gimenez <igimenez@vintra.io>
2019-10-08 18:48:12 +0200

    First try

---
+++ pyhello/__init__.py
@@ -0,0 +1,3 @@
+
+def say():
+   return (u'Hello World!')
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyhello',
+      version='0.1',
+      description='The funniest hello world in the world',
+      url='https://github.com/ivan-gimenez/pyhello.git',
+      author='Ivan Gimenez',
+      author_email='ivan.gimenez@example.com',
+      license='MIT',
+      packages=['pyhello'],
+      zip_safe=False)
